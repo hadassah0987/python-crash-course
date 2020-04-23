e091cbbd040a17652bb508725ece053ca1f1282c
Hadassah Dobner <hadassah0987@gmail.com>
2020-04-23 02:55:07 -0400

    FIXD

--- apostrophe.py
+++ apostrophe.py
@@ -1,7 +1,7 @@
 message = "One of Python's strengths is its diverse community."
 print(message)
 
-message = 'One of Python's strengths is its diverse community.
+message = "One of Python's strengths is its diverse community."
 print(message)
 
 
